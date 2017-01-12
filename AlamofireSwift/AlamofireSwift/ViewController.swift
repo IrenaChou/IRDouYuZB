93d9f1b8110b46c49890657b5f8a291a20c6462e
zhouning@incamel.com <zhouning@incamel.com>
2017-01-12 17:58:33 +0800

    test

--- AlamofireSwift/AlamofireSwift/ViewController.swift
+++ AlamofireSwift/AlamofireSwift/ViewController.swift
@@ -7,19 +7,40 @@
 //
 
 import UIKit
-
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-//        NetworkTools.requestData(type: .Get, URLString: "http://httpbin.org/get") { (result) in
-//            print(result)
+        
+        
+        let url = "http://capi.douyucdn.cn/api/v1/getHotCate"
+
+        //        //请求头
+        let header: HTTPHeaders = [
+//            "connection":"keep-alive",
+            "Charsert":"utf-8",
+            "Content-Type":"application/json",
+            "Accept-Encoding":"gzip, identity"
+        ]
+//        let body:Data? = Data(base64Encoded: "test")
+//        
+//       let result = Alamofire.upload(body!, to: url, method: HTTPMethod.get, headers: header)
+        
+//        Alamofire.request(url, method: HTTPMethod.get, headers: header).response { (response) in
+//            print(response)
 //        }
-        NetworkTools.requestData(type: MethodType.Get, URLString: "http://capi.douyucdn.cn/api/v1/getHotCate",parameter: ["limit":"4","offset":"0","time":"1484124821"]) { (result) in
-                print(result)
+        let reqMe = Alamofire.request(url, method: HTTPMethod.get, headers: header)
+        
+        
+        reqMe.response { (response) in
+            print(response)
         }
 
+        //        print("\(result)")
+//        Alamofire.upload( to: url, method: HTTPMethod.get, headers: header)
+    
     }
 
     override func didReceiveMemoryWarning() {
